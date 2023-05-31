840a85777e7fd3f7b329613901c0a5110f27d2d6
liviaher <liviaher.oliveira@gmail.com>
2023-05-31 03:21:24 -0300

    backend host added

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.BACKEND_URL
+    baseURL: "https://backend-production-08bb.up.railway.app"
 })
 
 export default api
